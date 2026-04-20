3a3ba63d0a3428094a536105d16f690b5cc7bbbb
Fernando <fernandogarciadelrio76@gmail.com>
2026-04-19 22:03:36 -0300

    agrega role a fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,24 +11,20 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable, TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
-     *
-     * @var list<string>
      */
     protected $fillable = [
         'name',
         'email',
         'password',
+        'role', // 👈 ESTA ES LA CLAVE
     ];
 
     /**
      * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
      */
     protected $hidden = [
         'password',
@@ -39,8 +35,6 @@ class User extends Authenticatable
 
     /**
      * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
      */
     protected function casts(): array
     {

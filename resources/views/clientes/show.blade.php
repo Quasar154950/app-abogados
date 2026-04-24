22c437147af9aa5e63edc765aa78d153e970d503
Fernando <fernandogarciadelrio76@gmail.com>
2026-04-23 23:54:04 -0300

    fix alineacion mobile ficha cliente

--- resources/views/clientes/show.blade.php
+++ resources/views/clientes/show.blade.php
@@ -152,6 +152,7 @@
         </form>
     @endif
 </div>
+        </div>
 
         {{-- 3. BLOQUE: DOCUMENTACIÓN DEL CLIENTE --}}
         <div class="rounded-xl border border-neutral-200 dark:border-neutral-700 p-6 bg-white dark:bg-neutral-900 shadow-sm text-left">

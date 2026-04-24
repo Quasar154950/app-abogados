c6342babaee6e60edb07cb83a8464f679aa5247c
Fernando <fernandogarciadelrio76@gmail.com>
2026-04-24 01:05:33 -0300

    fix centrado global layout mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,10 @@
         x-on:keydown.window.ctrl.k.prevent="$dispatch('open-global-search')"
         x-on:keydown.window.meta.k.prevent="$dispatch('open-global-search')"
     >
-        <flux:main>
-            {{ $slot }}
+        <flux:main class="px-4">
+            <div class="mx-auto w-full max-w-5xl">
+                {{ $slot }}
+            </div>
         </flux:main>
     </div>
 </x-layouts::app.sidebar>

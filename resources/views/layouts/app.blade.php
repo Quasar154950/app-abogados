c742e7998adffe1a70eef5a0bd41f749260cc183
Fernando <fernandogarciadelrio76@gmail.com>
2026-04-23 23:41:21 -0300

    elimina buscador global duplicado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,12 +1,11 @@
 <x-layouts::app.sidebar :title="$title ?? null">
-    {{-- Escuchador global de teclado --}}
-    <div x-data x-on:keydown.window.ctrl.k.prevent="$dispatch('open-global-search')" x-on:keydown.window.meta.k.prevent="$dispatch('open-global-search')">
-        
+    <div
+        x-data
+        x-on:keydown.window.ctrl.k.prevent="$dispatch('open-global-search')"
+        x-on:keydown.window.meta.k.prevent="$dispatch('open-global-search')"
+    >
         <flux:main>
             {{ $slot }}
         </flux:main>
-
-        {{-- Aquí insertaremos el componente del buscador en el siguiente paso --}}
-        <livewire:global-search />
     </div>
 </x-layouts::app.sidebar>

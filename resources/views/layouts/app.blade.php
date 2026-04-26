526da8630df8010c2d1bc8218c84f226ed37d2ba
Fernando <fernandogarciadelrio76@gmail.com>
2026-04-26 16:34:34 -0300

    fix ancho layout mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,8 @@
         x-on:keydown.window.ctrl.k.prevent="$dispatch('open-global-search')"
         x-on:keydown.window.meta.k.prevent="$dispatch('open-global-search')"
     >
-        <flux:main class="px-4">
-            <div class="w-full px-2 sm:px-3 md:mx-auto md:max-w-5xl md:px-4">
+        <flux:main class="px-2 sm:px-3 md:px-4">
+            <div class="w-full md:mx-auto md:max-w-5xl">
                 {{ $slot }}
             </div>
         </flux:main>

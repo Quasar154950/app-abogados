73ae8136634b830bf29025bd6dd6f22104dd2042
Fernando <fernandogarciadelrio76@gmail.com>
2026-05-02 13:19:54 -0300

    eliminar ruta migrar

--- routes/web.php
+++ routes/web.php
@@ -73,10 +73,5 @@ Route::get('/test-cloudinary', function () {
 
     return $result ? 'OK' : 'ERROR';
 });
-use Illuminate\Support\Facades\Artisan;
 
-Route::get('/migrar', function () {
-    Artisan::call('migrate', ['--force' => true]);
-    return 'Migraciones ejecutadas';
-});
 require __DIR__ . '/settings.php';

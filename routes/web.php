3f9acb348e427279d80edbd27e0b8c543f0381e5
Fernando <fernandogarciadelrio76@gmail.com>
2026-08-30 23:37:13 -0300

    Eliminar ruta temporal de migraciones

--- routes/web.php
+++ routes/web.php
@@ -339,12 +339,4 @@ Route::get('/crear-slug', function () {
 Route::post('/webhooks/mercadopago/saas', [MercadoPagoSaasWebhookController::class, 'handle'])
     ->name('webhooks.mercadopago.saas');
 
-Route::get('/migrar-estudios-temp', function () {
-    \Illuminate\Support\Facades\Artisan::call('migrate', [
-        '--force' => true,
-    ]);
-
-    return '<pre>' . \Illuminate\Support\Facades\Artisan::output() . '</pre>';
-});   
-
 require __DIR__ . '/settings.php';
